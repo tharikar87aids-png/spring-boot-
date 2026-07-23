aace343bf2df70b9e9dbaa9df3a69e7b88439ee9
Tharika R <tharikar87aids@dgct.ac.in>
2026-07-23 22:27:41 +0530

    Add files via upload

---
+++ Springboot/studentmanagement/src/main/java/com/tharika/studentmanagement/StudentmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.tharika.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementApplication.class, args);
+	}
+}
---
+++ Springboot/studentmanagement/src/main/java/entity/Student.java
@@ -0,0 +1,1 @@
+package com.tharika.studentmanagement.entity;
